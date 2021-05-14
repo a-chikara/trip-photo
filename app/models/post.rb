16ea206b4b58f1f676baac0313c0ef9446f90870
a-chikara <aiba102chikara@gmail.com>
2021-05-14 13:44:12 +0900

    postテーブル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
-  
+  with_options presence: true do
+    validates :city
+    validates :explain
+  end
 end
--- db/migrate/20210514043456_create_posts.rb
+++ db/migrate/20210514043456_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.text :city,               null: false
+      t.text :explain,            null: false
+    
       t.timestamps
     end
   end
