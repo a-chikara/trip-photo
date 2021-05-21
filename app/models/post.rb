e5322fe550f9e161f71397dbc9482340db4c0c9d
a-chikara <aiba102chikara@gmail.com>
2021-05-21 15:37:41 +0900

    1を選べないバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :city
+    validates :city_id, numericality: { other_than: 1 }
     validates :explain
   end
 
