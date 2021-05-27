c6811607c5f689d76156a8172fd0a5be03bb4e72
a-chikara <aiba102chikara@gmail.com>
2021-05-27 16:07:09 +0900

    usersルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
 
-
+  resources :users, only: [:edit, :update]
   resources :posts, only: [:index, :new, :create, :destroy]
 end
